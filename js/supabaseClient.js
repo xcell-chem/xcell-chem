1dee19d07851005d2ebf44c20172416fd5148205
xcell-chem <xcell.chem91@gmail.com>
2025-01-28 19:50:19 +0000

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -5,4 +5,9 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 export const SUPABASE_URL = 'https://tjbcucdewwczndkeypey.supabase.co';
 export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqYmN1Y2Rld3djem5ka2V5cGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5MzUwMzcsImV4cCI6MjA1MzUxMTAzN30.iBm2u7xY5qRQT6gOQw7OwAYTENJh49B9lI0YtLuKJAQ';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+    auth: {
+        persistSession: true, // Ensures sessions are persisted across page reloads
+        detectSessionInUrl: true, // Ensures the session is detected after OAuth login
+    },
+});
