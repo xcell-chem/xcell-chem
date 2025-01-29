c9762057db8c5de63e8c1112be2d96758bb1015d
xcell-chem <xcell.chem91@gmail.com>
2025-01-29 19:21:44 +0000

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "../node_modules/@supabase/supabase-js/dist/module/index.js";
 
 const supabaseUrl = "https://tjbcucdewwczndkeypey.supabase.co";
 const supabaseKey = "YOUR_SUPABASE_ANON_KEY";  // Replace with your actual key, preferably from environment variables
