4f3bbd4a42dc8e9b595038df7367c7ba8a0c7614
xcell-chem <xcell.chem91@gmail.com>
2025-01-30 02:38:19 +0000

    mm

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 
 const supabaseUrl = "https://tjbcucdewwczndkeypey.supabase.co";
-const supabaseKey = "YOUR_SUPABASE_ANON_KEY";  // Replace with your actual key
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqYmN1Y2Rld3djem5ka2V5cGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5MzUwMzcsImV4cCI6MjA1MzUxMTAzN30.iBm2u7xY5qRQT6gOQw7OwAYTENJh49B9lI0YtLuKJAQ";  // Replace with your actual key
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
